57c0caaeac56379f1dac89589d29e9c4a602b036
Alexander_Yulin <ordinaraviro@gmail.com>
2022-10-31 16:45:19 +0200

    refactror: change HTML, CSS, JS-formatting according to the rule 2.1 of the html-and-css-extended.md

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
-    listItem.className="tasks-list-item";
+    listItem.className="tasks__item";
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
-    checkBox.className="input-field";
+    checkBox.className="input-check-field";
     //label
     var label=document.createElement("label");//label
     //input (text)
@@ -39,12 +39,13 @@ var createNewTaskElement=function(taskString){
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="input-field task";
+    editInput.className="input-text-field task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="btn edit";
 
     deleteButton.className="btn delete";
+    deleteButtonImg.className="btn-delete-svg";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -83,10 +84,10 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector('.input-text-field');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
+    var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -100,7 +101,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    listItem.classList.toggle("edit-mode");
 };
 
 
